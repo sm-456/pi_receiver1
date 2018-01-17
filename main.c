321185689e291db6023703d07e3bf33a58e8ff09
smoehrin <smoehrin@stud.hs-offenburg.de>
2018-01-17 10:31:30 +0100

    tidying up code

--- main.c
+++ main.c
@@ -15,6 +15,12 @@
 #define PIN18_IRQ RPI_GPIO_P1_18
 #define PIN16_SDN RPI_GPIO_P1_16
 
+#define STATE_IDLE 0
+#define STATE_RX 1
+#define STATE_TX 2
+#define STATE_FILE 3
+#define STATE_REGISTRATION 4
+
 #define PLOAD 18
 #define FIFO 18
 #define RA 0
@@ -45,7 +51,7 @@ int main()
  =============================================================================*/
 	int i,j;
 	uint8_t tmp_ui8;
-	uint16_t val_bytes = 0;
+	uint16_t received_bytes = 0;
 	uint16_t tmp_sensor_id = 0;
 	uint16_t tmp_dataframe_id = 0;
 	uint16_t parameter = 0;
@@ -76,14 +82,14 @@ int main()
 	// oldest dataset at 0
 	uint32_t rx_time_array[RX_DATA_BUFFER] = {0};	// 32 bit UNIX timestamp
 	
-	uint8_t message_buffer[2][MEASURE_VALUES*2+4] = {0};  // save first packages until transmission complete
+	uint8_t received_packets_buffer[2][MEASURE_VALUES*2+4] = {0};  // save first packages until transmission complete
 	uint8_t time_table[MEASURE_VALUES][6] = {0};		  // array for time in csv data table
 	char string[100];
 
 /*==============================================================================
                                 FLAGS
  =============================================================================*/	
-	uint8_t ready = 0;
+	uint8_t go_ready_state = 0;
 	uint8_t spirit_on = 0;
 	uint8_t irq_rx_data_ready = 0;
 	uint8_t data_received = 0;
@@ -96,8 +102,8 @@ int main()
  =============================================================================*/
  
      uint64_t counter = 0;				// generic counter
-	 uint8_t dataset_counter = 0;		// number of received complete datasets	
-	 uint8_t package_counter = 0;		// number of received packages of single set (3 or 4)
+	 uint8_t stored_datasets_counter = 0;		// number of received complete datasets	
+	 uint8_t received_packets_counter = 0;		// number of received packages of single set (3 or 4)
 	
 	 //uint16_t *p_value_array;
 
@@ -124,7 +130,7 @@ int main()
     bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 	//delay(10);
 	
-	// HW pin 18 rising edge detect (RX ready)
+	// HW pin 18 rising edge detect (RX go_ready_state)
 	bcm2835_gpio_fsel(PIN18_IRQ, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_set_pud(PIN18_IRQ, BCM2835_GPIO_PUD_DOWN);
     bcm2835_gpio_hen(PIN18_IRQ);
@@ -168,7 +174,7 @@ int main()
 	
 	while(1)
 	{
-		if(state == 1) //rx
+		if(state == STATE_RX) //rx
 		{
 			if(spirit_on == 0)
 			{
@@ -235,29 +241,29 @@ int main()
 				irq_rx_data_ready = 0;
 				
 				printf("data received!\n");
-				val_bytes = SpiritLinearFifoReadNumElementsRxFifo();
-				printf("No of elements: %d\n", val_bytes);
-				SpiritSpiReadLinearFifo(val_bytes, rx_buffer);
+				received_bytes = SpiritLinearFifoReadNumElementsRxFifo();
+				printf("No of elements: %d\n", received_bytes);
+				SpiritSpiReadLinearFifo(received_bytes, rx_buffer);
 				
 				SpiritCmdStrobeFlushRxFifo();
 						
 				more_data = rx_buffer[2]&0x01;	// extract last bit
 				
-				for(i=0;i<val_bytes;i++)
+				for(i=0;i<received_bytes;i++)
 				{
 					printf("%X ", rx_buffer[i]);
 				}
 				printf("\n");
 				
-				if(val_bytes == (MEASURE_VALUES*2 + 4))
+				if(received_bytes == (MEASURE_VALUES*2 + 4))
 				{
 					data_ok = 1;
 					/*
-					dataset_counter++;
-					if(dataset_counter == RX_DATA_BUFFER)
+					stored_datasets_counter++;
+					if(stored_datasets_counter == RX_DATA_BUFFER)
 					{
 						data_write = 1;
-						dataset_counter = 0;
+						stored_datasets_counter = 0;
 					}
 					*/
 				} 
@@ -271,7 +277,7 @@ int main()
 						for(j=0;j<2;j++)
 						{			
 							// sort data
-							for(i=0;i<val_bytes;i=i+2)
+							for(i=0;i<received_bytes;i=i+2)
 							{
 								tmp_ui8 = rx_buffer[i];
 								rx_buffer[i] = rx_buffer[i+1];
@@ -284,26 +290,26 @@ int main()
 							parameter = tmp_sensor_id & 0x001F;
 							
 							// write data to value array
-							for(i=4;i<val_bytes;i=i+2)
+							for(i=4;i<received_bytes;i=i+2)
 							{
 								switch(parameter)
 								{
 									case 1: 
-										temperature[dataset_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
+										temperature[stored_datasets_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
 									case 2: 
-										pressure[dataset_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
+										pressure[stored_datasets_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
 									case 3: 
-										humidity[dataset_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
+										humidity[stored_datasets_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
 									case 4:
-										moisture[dataset_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
+										moisture[stored_datasets_counter][(i/2)-2] = (rx_buffer[i]<<8)|rx_buffer[i+1]; break;
 									default: break;
 								}
 							}
 							
 							// prepare new array from buffer
 							if(j>0)
-								package_counter--;
-							memcpy(&(message_buffer[j][0]),rx_buffer,MEASURE_VALUES*2+4);
+								received_packets_counter--;
+							memcpy(&(received_packets_buffer[j][0]),rx_buffer,MEASURE_VALUES*2+4);
 							
 						}
 					}
@@ -330,13 +336,13 @@ int main()
 					
 					//====================================================
 					
-					rx_time_array[dataset_counter] = (uint32_t)t_rx;	// save UNIX timestamp
+					rx_time_array[stored_datasets_counter] = (uint32_t)t_rx;	// save UNIX timestamp
 								
-					dataset_counter++;
-					if(dataset_counter == RX_DATA_BUFFER)
+					stored_datasets_counter++;
+					if(stored_datasets_counter == RX_DATA_BUFFER)
 					{
 						data_write = 1;
-						dataset_counter = 0;
+						stored_datasets_counter = 0;
 					}
 					
 					// all data stored?	
@@ -347,23 +353,23 @@ int main()
 					// go back to idle
 					
 					if(data_write == 0)
-						state = 0;
+						state = STATE_IDLE;
 					else
-						state = 3;
+						state = STATE_FILE;
 					
 				}
 				else  // more packages to be received
 				{
-					package_counter++;
+					received_packets_counter++;
 					// copy data to buffer
-					memcpy(rx_buffer,&(message_buffer[package_counter-1][0]),2*MEASURE_VALUES+4);		
+					memcpy(rx_buffer,&(received_packets_buffer[received_packets_counter-1][0]),2*MEASURE_VALUES+4);		
 					// return to rx mode
 				}
 			}
 			
 		}
 		
-		if(state == 3)	// write to file
+		if(state == STATE_FILE)	// write to file
 		{
 			fp = fopen(filep, "a+");
 			// dataset loop (buffered messages, each contains all 4 measurement values)
@@ -394,15 +400,15 @@ int main()
 			}
 			
 			fclose(fp);
-			dataset_counter = 0;
-			state = 0;
+			stored_datasets_counter = 0;
+			state = STATE_IDLE;
 			
 			
 			
 			
 		}
 		
-		if(state == 2) //tx
+		if(state == STATE_TX) //tx
 		{	
 			if(spirit_on == 0)
 			{
@@ -465,19 +471,19 @@ int main()
 				bcm2835_gpio_write(PIN16_SDN, HIGH);
 				delay(2000);
 				// go back to idle
-				state = 0;
+				state = STATE_IDLE;
 			}
 			
 		}
 		
 		
 		
-		if(ready == 1)
+		if(go_ready_state == 1)
 		{
 			SpiritRefreshStatus();
 			if(g_xStatus.MC_STATE != MC_STATE_READY)
 			{
-				// set the ready state 
+				// set the go_ready_state state 
 				//SpiritCmdStrobeSabort();
 				do
 				{ 
@@ -494,12 +500,12 @@ int main()
 				}while(g_xStatus.MC_STATE!=MC_STATE_READY);	
 
 			}
-			printf("State: ready\n");
+			printf("State: go_ready_state\n");
 
-			ready = 0;
+			go_ready_state = 0;
 		}
 
-		if(state == 0)
+		if(state == STATE_IDLE)
 		{
 			t = time(NULL);
 			ts = localtime(&t);
@@ -508,14 +514,14 @@ int main()
 			//delay(1000);
 			if((ts->tm_sec % 10) == 9)
 			{
-				state = 1;	// rx
+				state = STATE_RX;	// rx
 				printf("Time: %d seconds. Start RX mode\n",ts->tm_sec);
 			}
 			else
 			{
 				if((ts->tm_sec % 10) == 11)
 				{
-					state = 2;	// tx
+					state = STATE_TX;	// tx
 					printf("Time: %d seconds. Start TX mode\n",ts->tm_sec);
 				}
 				else
